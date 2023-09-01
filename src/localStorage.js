953a34646c91714f9db459ec14770cf1a7e1a1c6
roopxx <127219908+roopxx@users.noreply.github.com>
2023-09-02 02:30:55 +0530

    feat: Add Storage class for local data persistence
    Introduced a Storage class responsible for saving and retrieving the
    to-do list data from local storage. This class includes methods for
    saving and loading the to-do list, projects, and tasks.

---
+++ src/localStorage.js
@@ -0,0 +1,89 @@
+import Project from "./project";
+import Task from "./task";
+import TodoList from "./todolist";
+
+export default class Storage {
+  static saveTodoList(data) {
+    localStorage.setItem("todoList", JSON.stringify(data));
+  }
+
+  static getTodoList() {
+    const todoList = Object.assign(
+      new TodoList(),
+      JSON.parse(localStorage.getItem("todoList"))
+    );
+
+    todoList.setProjects(
+      todoList
+        .getProjects()
+        .map((project) => Object.assign(new Project(), project))
+    );
+
+    todoList.getProjects().forEach((project) => {
+      project.setTasks(
+        project.getTasks().map((task) => Object.assign(new Task(), task))
+      );
+    });
+
+    return todoList;
+  }
+
+  static addProject(project) {
+    const todoList = Storage.getTodoList();
+    todoList.addProject(project);
+    Storage.saveTodoList(todoList);
+  }
+
+  static deleteProject(projectName) {
+    const todoList = Storage.getTodoList();
+    todoList.deleteProject(projectName);
+    Storage.saveTodoList(todoList);
+  }
+
+  static addTask(projectName, task) {
+    const todoList = Storage.getTodoList();
+    todoList.getProject(projectName).addTask(task);
+    Storage.saveTodoList(todoList);
+  }
+
+  static toggleTaskCompleted(projectName, taskName) {
+    const todoList = Storage.getTodoList();
+    const tasks = todoList.getProject(projectName).getTasks();
+    const taskIndex = tasks.findIndex((task) => task.name === taskName);
+
+    if (taskIndex !== -1) {
+      tasks[taskIndex].completed = !tasks[taskIndex].completed;
+      Storage.saveTodoList(todoList);
+    }
+  }
+
+  static deleteTask(projectName, taskName) {
+    const todoList = Storage.getTodoList();
+    todoList.getProject(projectName).deleteTask(taskName);
+    Storage.saveTodoList(todoList);
+  }
+
+  static renameTask(projectName, taskName, newTaskName) {
+    const todoList = Storage.getTodoList();
+    todoList.getProject(projectName).getTask(taskName).setName(newTaskName);
+    Storage.saveTodoList(todoList);
+  }
+
+  static setTaskDate(projectName, taskName, newDueDate) {
+    const todoList = Storage.getTodoList();
+    todoList.getProject(projectName).getTask(taskName).setDate(newDueDate);
+    Storage.saveTodoList(todoList);
+  }
+
+  static updateTodayProject() {
+    const todoList = Storage.getTodoList();
+    todoList.updateTodayProject();
+    Storage.saveTodoList(todoList);
+  }
+
+  static updateWeekProject() {
+    const todoList = Storage.getTodoList();
+    todoList.updateWeekProject();
+    Storage.saveTodoList(todoList);
+  }
+}
